8c6eebc428cf9fca13464e01d3a2393f4061a4e5
salmanfxrsi <salmanxprivate@gmail.com>
2025-05-08 00:26:34 +0600

    feat: solve problem 2 of TypeScript assignment

--- solutions.ts
+++ solutions.ts
@@ -2,3 +2,7 @@ function formatString(input: string, toUpper?: boolean): string {
     if(toUpper || toUpper == undefined) return input.toUpperCase();
     else return input.toLowerCase();
 }
+
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+    return items.filter (item  => item.rating >= 4);
+}
