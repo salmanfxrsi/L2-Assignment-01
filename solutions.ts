925f86a22beabe20e33a3007633bb36345f0624a
salmanfxrsi <salmanxprivate@gmail.com>
2025-05-08 00:39:49 +0600

    feat: solve problem 6 of TypeScript assignment

--- solutions.ts
+++ solutions.ts
@@ -6,3 +6,14 @@ function formatString(input: string, toUpper?: boolean): string {
 function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
     return items.filter (item  => item.rating >= 4);
 }
+
+interface Product {
+    name: string;
+    price: number;
+  }
+  
+function getMostExpensiveProduct(products: Product[]): Product | null {
+    if(products.length == 0) return null;
+
+    return products.reduce((current, max) => current.price > max.price ? current : max);
+}
